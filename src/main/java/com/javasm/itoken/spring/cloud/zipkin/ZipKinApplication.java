3807fafbf3c936982d9b9c377a183be166ce8bcd
user399031 <1120264714@qq.com>
2018-12-09 23:35:37 +0800

    修改了pom的项目名

---
+++ src/main/java/com/javasm/itoken/spring/cloud/zipkin/ZipKinApplication.java
@@ -0,0 +1,9 @@
+package com.javasm.itoken.spring.cloud.zipkin;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ZipKinApplication {
+}
